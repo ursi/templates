ac024d317092ba2261dea9ba0b3221bece59f5e9
Mason Mackaman <masondeanm@aol.com>
2019-10-11 14:42:31 -0400

    Fix whitespace

--- electron/src/app.js
+++ electron/src/app.js
@@ -2,8 +2,8 @@ const
 	fs = require('fs');
 	path = require('path'),
 	{
-	    app,
-	    BrowserWindow,
+		app,
+		BrowserWindow,
 		Menu,
 		MenuItem,
 	} = require('electron');
@@ -19,30 +19,30 @@ const
 //].forEach(db => {if (!fs.existsSync(db)) fs.writeFileSync(db, /*`{}` or `[]`*/);});
 
 function createWindow() {
-    let win = new BrowserWindow({
+	let win = new BrowserWindow({
 		title: `TITLE`,
-        width: 1900,
-        height: 1000,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+		width: 1900,
+		height: 1000,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-    //win.removeMenu();
-    win.loadFile(path.join('win', 'index.html'));
-    win.webContents.on('before-input-event', (event, input) => {
-        switch (input.type) {
-            case 'keyDown':
-                switch (input.key) {
-                    case 'F12':
-                        win.webContents.toggleDevTools();
+	//win.removeMenu();
+	win.loadFile(path.join('win', 'index.html'));
+	win.webContents.on('before-input-event', (event, input) => {
+		switch (input.type) {
+			case 'keyDown':
+				switch (input.key) {
+					case 'F12':
+						win.webContents.toggleDevTools();
 						//win.webContents.addWorkSpace(__dirname); // keep
 						break;
-                    case 'F5':
+					case 'F5':
 						win.webContents.reload();
-                } break;
-        }
-    });
+				} break;
+		}
+	});
 
 	win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
 		callback({responseHeaders: Object.fromEntries(Object.entries(details.responseHeaders).filter(header => !/x-frame-options/i.test(header[0])))});
